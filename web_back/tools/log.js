8023605a9d6e1928cdccca5b03c1c577488e6e55
jacky-01-wang <1635136152@qq.com>
2019-12-16 17:39:19 +0800

    登陆页面

---
+++ web_back/tools/log.js
@@ -0,0 +1,3 @@
+var BASELOG='http://localhost:8000/';
+var LOGIN=BASELOG+'admin/login';
+var LOGOUT=BASELOG+'admin/logout';
---
+++ web_back/tools/user.js
@@ -0,0 +1,33 @@
+var user={
+    login: function (options) {
+        $.ajax({
+            type:'post',
+            url:LOGIN,
+            data:{
+                user_name:options.data.username,
+                password:options.data.password
+            },
+            success: function (res) {
+                if (res.code==200) {
+                   options.success()
+                }else{
+                   options.fail()
+                }
+            }
+    
+        })
+    },
+    logout: function (options) {
+        $.ajax({
+            type:'post',
+            url:LOGOUT,
+            success: function (res) {
+                if (res.code==200) {
+                    options.success()
+                }else{
+                   options.fail()
+                }
+            }
+        })
+    }
+}
